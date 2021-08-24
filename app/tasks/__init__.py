e8ecd87e26f93c73fa8bd613eff79e54cf4ba900
Wijatama <44823545+SokKanaTorajd@users.noreply.github.com>
2021-08-24 15:44:00 +0700

    update broker_url

--- app/tasks/__init__.py
+++ app/tasks/__init__.py
@@ -2,7 +2,7 @@ from celery import Celery
 from app.models import config
 
 worker = Celery(__name__)
-worker.conf.broker_url = config.MONGODB_URI
+worker.conf.broker_url = config.BROKER_URL
 worker.conf.result_backend = config.MONGODB_URI
 worker.conf.mongodb_backend_settings = {
         'database': 'system_log',
