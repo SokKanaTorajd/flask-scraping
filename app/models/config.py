8504a4eaaff650625ffdfcb7fe6b80dac41911fd
torajd <t.wijatama.d@gmail.com>
2021-08-25 19:55:42 +0700

    change to rabbitamqp

--- app/models/config.py
+++ app/models/config.py
@@ -1,6 +1,6 @@
 import os
 
-BROKER_URL = os.environ.get('BROKER_URI')
+BROKER_URL = os.environ.get('CLOUDAMQP_URL')
 MONGODB_URI = os.environ.get('MONGODB_URI')
 BACKEND_URI = os.environ.get('BACKEND_URI')
 DB_NAME = os.environ.get('DB_NAME')
